dde760fa81e99805a90bd88766bb47eac1ec6ac3
Cheoun Sithea <cheoun.sithea@gmail.com>
2022-12-14 21:52:39 +0700

    Fixed

---
+++ lib/screen/nearMeWidget.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NearMeWidget extends StatelessWidget {
+  const NearMeWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
